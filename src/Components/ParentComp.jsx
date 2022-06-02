b71d1ea9ebc041d9ac7d088fa19242936bf2c478
Kapil Sharma <kapil.pundit@gmail.com>
2022-06-02 21:39:23 +0530

    Typo correction

--- src/Components/ParentComp.jsx
+++ src/Components/ParentComp.jsx
@@ -24,7 +24,7 @@ class ParentComp extends Component {
         console.log("Parent component render()");
         return (
             <div>
-                I am the Parent Componen!
+                I am the Parent Component!
                 <RegularComp name={this.state.name} />
                 <PureComp name={this.state.name} />
             </div>
